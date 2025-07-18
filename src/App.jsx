f857b7ca6851d66416ee1c6b2f8d36503e3eec6f
nicolamarino <nicolamarino527@gmail.com>
2025-07-18 12:45:02 +0200

    create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
